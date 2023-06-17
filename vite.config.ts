1162eee7b9170ff806265f6928d073dca85f2398
heungjun.park <rockheung@gmail.com>
2023-06-17 20:59:12 +0900

    feat: add proxy setting for target webpage

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import fs from "node:fs";
+import react from "@vitejs/plugin-react";
+import { responseInterceptor } from "http-proxy-middleware";
+
+const TARGET_HOST = "https://bluemoon100.imtest.me";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync("./certs/localhost-key.pem"),
+      cert: fs.readFileSync("./certs/localhost.pem"),
+    },
+    proxy: {
+      "^.*$": {
+        target: TARGET_HOST,
+        changeOrigin: true,
+        cookieDomainRewrite: {
+          "*": "",
+        },
+        selfHandleResponse: true,
+        configure: (proxy, options) => {
+          proxy.on("proxyReq", (proxyReq, req, res) => {
+            proxyReq.setHeader("referer", options.target + "/");
+          });
+          proxy.on(
+            "proxyRes",
+            responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+              const response = responseBuffer.toString("utf8");
+              return response.replace("Blue Moon", "Goodbye");
+            })
+          );
+        },
+      },
+    },
+  },
+});
