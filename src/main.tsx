3a36e965b6376a6954dc6dbdf1169b76e8c5fe3b
heungjun.park <rockheung@gmail.com>
2023-06-19 14:45:54 +0900

    chore: update id

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('shop_cart_list') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
