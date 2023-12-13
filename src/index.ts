6f74985f0dd731154184607cde690a919fdd6873
Rockheung <rockheung@gmail.com>
2023-12-14 03:22:57 +0900

    chore: refact

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Plugin } from "vite";
 import fs from "node:fs/promises";
 import { responseInterceptor } from "http-proxy-middleware";
 import { JSDOM } from "jsdom";
+import http from "node:http";
 
 interface ReactPageOnLiveOptions {
   // Target page to inject React app
@@ -23,14 +24,15 @@ interface ReactPageOnLiveOptions {
   // CSS selector to ignore
   removeTargetSelectors?: string;
   // Override header
-  overrideHeaders?: Record<string, string>;
+  headersOverridden?: Record<string, string>;
 }
 
 const DEFAULT_ROOT_ID = "root";
 const DEFAULT_MAIN_APP_SRC = "src/main.tsx";
 
-
-async function reactPageOnLive(userOptions: ReactPageOnLiveOptions): Promise<Plugin> {
+async function reactPageOnLive(
+  userOptions: ReactPageOnLiveOptions
+): Promise<Plugin> {
   const ignoreProxyPaths = [
     "node_modules", // node_modules/vite/dist/client/env.mjs
     "@vite/client", // HMR stuff
@@ -41,7 +43,46 @@ async function reactPageOnLive(userOptions: ReactPageOnLiveOptions): Promise<Plu
     .map((fileName) => `(?!/${fileName})`)
     .join("");
 
-    // TODO: If userOptions.livePageOrigin's scheme is https, use server option with https.
+  const rewriteReferer = (
+    proxyReq: http.ClientRequest,
+    req: http.IncomingMessage
+  ) => {
+    if (typeof req.headers.referer === "undefined") return;
+    const { protocol, host } = new URL(req.headers.referer);
+    const { protocol: protocolTarget, host: hostTarget } = new URL(
+      userOptions.livePageOrigin
+    );
+    proxyReq.setHeader(
+      "referer",
+      req.headers.referer
+        .replace(protocol, protocolTarget)
+        .replace(host, hostTarget)
+    );
+  };
+
+  const overrideHeaders = (proxyReq: http.ClientRequest) => {
+    if (typeof userOptions.headersOverridden !== "undefined") {
+      for (const [key, value] of Object.entries(
+        userOptions.headersOverridden
+      )) {
+        proxyReq.setHeader(key, value);
+      }
+    }
+  };
+
+  // INFO: Accept header for document request per browser
+  // Chrome:  text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+  // Firefox: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
+  // Safari:  text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+  // Edge:    text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+  const isDocumentRequest = (
+    proxyRes: http.IncomingMessage,
+    req: http.IncomingMessage
+  ) =>
+    /text\/html/i.test(proxyRes.headers["content-type"] ?? "") &&
+    /text\/html/i.test(req.headers.accept ?? "");
+
+  // TODO: If userOptions.livePageOrigin's scheme is https, use server option with https.
   return {
     name: "live-page-on-live",
     config: async ({ build, server }) => {
@@ -56,117 +97,104 @@ async function reactPageOnLive(userOptions: ReactPageOnLiveOptions): Promise<Plu
               },
               selfHandleResponse: true,
               configure: (proxy, options) => {
-                proxy.on("proxyReq", (proxyReq, req) => {
-                  if (typeof req.headers.referer === 'undefined') return;
-                  const refererPathname = new URL(req.headers.referer || options.target as string).pathname;
-                  proxyReq.setHeader("referer", options.target + refererPathname);
-                  if (typeof userOptions.overrideHeaders !== 'undefined') {
-                    for (const [key, value] of Object.entries(userOptions.overrideHeaders)) {
-                      proxyReq.setHeader(key, value);
-                    }
-                  }
-                });
+                proxy.on("proxyReq", rewriteReferer);
+                proxy.on("proxyReq", overrideHeaders);
                 proxy.on(
                   "proxyRes",
-                  responseInterceptor(
-                    async (responseBuffer, proxyRes, req) => {
-                      if (
-                        typeof proxyRes.headers['content-type'] !==
-                          'undefined' &&
-                        !/^text\/html/.test(proxyRes.headers['content-type'])
-                      ) {
-                        return responseBuffer
+                  responseInterceptor(async (responseBuffer, proxyRes, req) => {
+                    if (!isDocumentRequest(proxyRes, req)) {
+                      return responseBuffer;
+                    }
+
+                    if (
+                      typeof userOptions.ignorePathRegex !== "undefined" &&
+                      typeof req.url === "string" &&
+                      new RegExp(userOptions.ignorePathRegex).test(req.url)
+                    ) {
+                      return responseBuffer;
+                    }
+
+                    const scheme = server?.https ? "https" : "http";
+
+                    const {
+                      window: { document },
+                    } = new JSDOM(responseBuffer, {
+                      url: options.target as string,
+                      referrer: options.target as string,
+                      contentType: "text/html; charset=utf-8" as any,
+                    });
+
+                    const targetNodes =
+                      typeof userOptions.removeTargetSelectors === "string"
+                        ? document.querySelectorAll(
+                            userOptions.removeTargetSelectors
+                          )
+                        : [];
+
+                    for (const dom of targetNodes) {
+                      if (dom) {
+                        dom.remove();
                       }
+                    }
 
-                      if (
-                        typeof userOptions.ignorePathRegex !== 'undefined' &&
-                        typeof req.url === 'string' &&
-                        new RegExp(userOptions.ignorePathRegex).test(req.url)
-                      ) {
-                        return responseBuffer
-                      }
+                    const appRootNode = document.getElementById(
+                      userOptions.appContainerId || DEFAULT_ROOT_ID
+                    );
 
-                      const scheme = server?.https ? "https" : "http";
-
-                      const {
-                        window: { document },
-                      } = new JSDOM(responseBuffer, {
-                        url: options.target as string,
-                        referrer: options.target as string,
-                        contentType: "text/html; charset=utf-8" as any
-                      });
-
-                      const targetNodes =
-                        typeof userOptions.removeTargetSelectors === "string"
-                          ? document.querySelectorAll(
-                              userOptions.removeTargetSelectors
-                            )
-                          : [];
-
-                      for (const dom of targetNodes) {
-                        if (dom) {
-                          dom.remove();
-                        }
-                      }
-
-                      const appRootNode = document.getElementById(
-                        userOptions.appContainerId || DEFAULT_ROOT_ID
+                    if (
+                      (typeof userOptions.appContainerId === "undefined" ||
+                        appRootNode === null) &&
+                      userOptions.forceMount === true
+                    ) {
+                      console.warn(
+                        "You should set `appContainerId` option to inject React app or default is " +
+                          DEFAULT_ROOT_ID
                       );
+                      console.warn(
+                        "If this message shows multiple times, server's response might be malformed."
+                      );
+                      console.warn(
+                        "This plugin does not support html ajax response."
+                      );
+                      console.warn(
+                        "You may need to manually ignore next ajax path with ignorePathRegex option."
+                      );
+                      console.warn("- " + req.url);
 
-                      if (
-                        (typeof userOptions.appContainerId === "undefined" ||
-                          appRootNode === null) &&
-                        userOptions.forceMount === true
-                      ) {
-                        console.warn(
-                          "You should set `appContainerId` option to inject React app or default is " +
-                            DEFAULT_ROOT_ID
+                      const targetNode = document.querySelector(
+                        userOptions.mountNextTo || "body > *:first-child"
+                      );
+                      if (targetNode === null) {
+                        console.log(
+                          "This request does not seem to be html request."
                         );
-                        console.warn(
-                          "If this message shows multiple times, server's response might be malformed."
-                        );
-                        console.warn(
-                          "This plugin does not support html ajax response."
-                        );
-                        console.warn(
-                          "You may need to manually ignore next ajax path with ignorePathRegex option."
-                        );
-                        console.warn("- " + req.url);
-
-                        const targetNode = document.querySelector(
-                          userOptions.mountNextTo || "body > *:first-child"
-                        );
-                        if (targetNode === null) {
-                          console.log(
-                            "This request does not seem to be html request."
-                          );
-                          return responseBuffer;
-                        }
-                        const appRoot = document.createElement("div");
-                        appRoot.id =
-                          userOptions.appContainerId || DEFAULT_ROOT_ID;
-                        targetNode.parentElement!.insertBefore(
-                          appRoot,
-                          targetNode.nextSibling
-                        );
+                        return responseBuffer;
                       }
-                      
-                      const warnMsg =
-                        "==================== vite script injected ====================";
-                      const devServerHost =
-                        userOptions.viteDevServerHost ||
-                        // based on vite server option
-                        server?.host === true
-                          ? "0.0.0.0"
-                          : server?.host || "localhost";
-                      const devServerPort =
-                        userOptions.viteDevServerPort || server?.port || 5173;
+                      const appRoot = document.createElement("div");
+                      appRoot.id =
+                        userOptions.appContainerId || DEFAULT_ROOT_ID;
+                      targetNode.parentElement!.insertBefore(
+                        appRoot,
+                        targetNode.nextSibling
+                      );
+                    }
 
-                      return (
-                        "<!DOCTYPE html>\n" +
-                        document.documentElement.outerHTML.replace(
-                          "</body>",
-                          `<script>console.warn('${warnMsg}')</script>
+                    const warnMsg =
+                      "==================== vite script injected ====================";
+                    const devServerHost =
+                      userOptions.viteDevServerHost ||
+                      // based on vite server option
+                      server?.host === true
+                        ? "0.0.0.0"
+                        : server?.host || "localhost";
+                    const devServerPort =
+                      userOptions.viteDevServerPort || server?.port || 5173;
+
+                    return (
+                      "<!DOCTYPE html>\n" +
+                      document.documentElement.outerHTML.replace(
+                        "</body>",
+                        `<script>console.warn('${warnMsg}')</script>
   <script type="module">  
     import RefreshRuntime from '${scheme}://${devServerHost}:${devServerPort}/@react-refresh'
     RefreshRuntime.injectIntoGlobalHook(window)
@@ -181,7 +209,9 @@ async function reactPageOnLive(userOptions: ReactPageOnLiveOptions): Promise<Plu
     }');
     if (container instanceof HTMLElement) {
       import("/${
-        build?.rollupOptions?.input || userOptions.mainAppSrc || DEFAULT_MAIN_APP_SRC
+        build?.rollupOptions?.input ||
+        userOptions.mainAppSrc ||
+        DEFAULT_MAIN_APP_SRC
       }").catch(console.error);
     } else {
       console.error('Container element not found: ', '${
@@ -190,10 +220,9 @@ async function reactPageOnLive(userOptions: ReactPageOnLiveOptions): Promise<Plu
     }
   </script>
 </body>`
-                        )
-                      );
-                    }
-                  )
+                      )
+                    );
+                  })
                 );
               },
             },
@@ -202,6 +231,6 @@ async function reactPageOnLive(userOptions: ReactPageOnLiveOptions): Promise<Plu
       };
     },
   };
-};
+}
 
 export default reactPageOnLive;
